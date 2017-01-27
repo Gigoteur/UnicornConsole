28c0d1c2b4cdf2ee204c0f6066ad017855c2965f
Hallucino <d@t0t0.fr>
2017-01-27 13:44:14 +0100

    up

--- ci/script.sh
+++ ci/script.sh
@@ -1,10 +1,21 @@
 set -ex
 
 main() {
+    cargo build --release
+    cargo build --release --features="sdl_renderer"
+    cargo build --release --features="cpython"
+    cargo build --release --features="lua"
+    cargo build --release --features="dyon"
+    cargo build --release --features="sdl_renderer cpython lua dyon"
+}
+
+main_deploy() {
     docker run --rm --privileged multiarch/qemu-user-static:register --reset
     docker build -t px8-$TARGET ci/docker/$TARGET
 }
 
-if [ -z $DEPLOY ]; then
+if [! -z $DEPLOY ]; then
+    main
+else
     main
 fi
